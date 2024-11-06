a63f724e5018234a60369d2821d0c082ebf3d596
Antonio Pardo <apardo@spluca.org>
2024-11-06 15:37:53 +0100

    firebaseConfig data not in the repo

--- firebase.ts
+++ firebase.ts
@@ -1,6 +1,15 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
+import {
+  API_KEY,
+  AUTH_DOMAIN,
+  PROJECT_ID,
+  STORAGE_BUCKET,
+  MESSAGING_SENDER_ID,
+  APP_ID
+} from "@env";
+
 // Optionally import the services that you want to use
 // import {...} from "firebase/auth";
 // import {...} from "firebase/database";
@@ -8,15 +17,14 @@ import { getAuth } from "firebase/auth";
 // import {...} from "firebase/functions";
 // import {...} from "firebase/storage";
 
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCskuM6Yzm7brpSDWX0I42bh8ndqNNKam0",
-  authDomain: "club-ee9e6.firebaseapp.com",
-  projectId: "club-ee9e6",
-  storageBucket: "club-ee9e6.firebasestorage.app",
-  messagingSenderId: "756200297833",
-  appId: "1:756200297833:web:92e620ee56227083f159f8",
-  measurementId: "G-1V8BR8Q6HZ"
+  apiKey: API_KEY,
+  authDomain: AUTH_DOMAIN,
+  projectId: PROJECT_ID,
+  storageBucket: STORAGE_BUCKET,
+  messagingSenderId: MESSAGING_SENDER_ID,
+  appId: APP_ID
 };
 
 // Initialize Firebase
